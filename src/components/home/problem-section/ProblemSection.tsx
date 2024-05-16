ac13b26aac21d00b5e9fb00e092e928b1bf509d6
Besufekad <besufkeat@gmail.com>
2024-05-16 10:10:44 +0300

    Remove map animation

--- src/components/home/problem-section/ProblemSection.tsx
+++ src/components/home/problem-section/ProblemSection.tsx
@@ -1,75 +1,75 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import Description from './Description';
 import Map from './Map';
-import { useInView } from 'react-intersection-observer';
-import MapFilled from './MapFilled';
+// import { useInView } from 'react-intersection-observer';
+// import MapFilled from './MapFilled';
 
 const ProblemSection = () => {
-  const { ref, inView } = useInView({
-    /* Options for the IntersectionObserver */
-    threshold: 0,
-  });
+  // const { ref, inView } = useInView({
+  //   /* Options for the IntersectionObserver */
+  //   threshold: 0,
+  // });
 
-  const [isCentered, setIsCentered] = useState(false);
+  // const [isCentered, setIsCentered] = useState(false);
 
-  const checkIfCentered = () => {
-    const element = document.getElementById('mapWrapper');
-    if (element) {
-      const rect = element.getBoundingClientRect();
-      const windowHeight =
-        window.innerHeight || document.documentElement.clientHeight;
-      const elementCenter = rect.top + rect.height / 2;
-      const isElementCentered =
-        elementCenter >= windowHeight / 2 - 50 &&
-        elementCenter <= windowHeight / 2 + 50;
-      setIsCentered(isElementCentered);
-    }
-  };
+  // const checkIfCentered = () => {
+  //   const element = document.getElementById('mapWrapper');
+  //   if (element) {
+  //     const rect = element.getBoundingClientRect();
+  //     const windowHeight =
+  //       window.innerHeight || document.documentElement.clientHeight;
+  //     const elementCenter = rect.top + rect.height / 2;
+  //     const isElementCentered =
+  //       elementCenter >= windowHeight / 2 - 50 &&
+  //       elementCenter <= windowHeight / 2 + 50;
+  //     setIsCentered(isElementCentered);
+  //   }
+  // };
 
-  const handleWheel = (e: WheelEvent) => {
-    e.preventDefault();
-    const element = document.getElementById('mapWrapper');
-    if (element) {
-      element.scrollTop += e.deltaY;
-      const atTop = element.scrollTop === 0;
-      const atBottom =
-        element.scrollHeight - element.scrollTop === element.clientHeight;
-      if (atTop || atBottom) {
-        document.body.removeEventListener('wheel', handleWheel);
-        document.body.classList.remove('no-scroll');
-      } else {
-        document.body.classList.add('no-scroll');
-      }
-    }
-  };
+  // const handleWheel = (e: WheelEvent) => {
+  //   e.preventDefault();
+  //   const element = document.getElementById('mapWrapper');
+  //   if (element) {
+  //     element.scrollTop += e.deltaY;
+  //     const atTop = element.scrollTop === 0;
+  //     const atBottom =
+  //       element.scrollHeight - element.scrollTop === element.clientHeight;
+  //     if (atTop || atBottom) {
+  //       document.body.removeEventListener('wheel', handleWheel);
+  //       document.body.classList.remove('no-scroll');
+  //     } else {
+  //       document.body.classList.add('no-scroll');
+  //     }
+  //   }
+  // };
 
-  useEffect(() => {
-    if (inView) {
-      checkIfCentered();
-      window.addEventListener('scroll', checkIfCentered);
-    } else {
-      window.removeEventListener('scroll', checkIfCentered);
-    }
-    return () => {
-      window.removeEventListener('scroll', checkIfCentered);
-    };
-  }, [inView]);
+  // useEffect(() => {
+  //   if (inView) {
+  //     checkIfCentered();
+  //     window.addEventListener('scroll', checkIfCentered);
+  //   } else {
+  //     window.removeEventListener('scroll', checkIfCentered);
+  //   }
+  //   return () => {
+  //     window.removeEventListener('scroll', checkIfCentered);
+  //   };
+  // }, [inView]);
 
-  useEffect(() => {
-    if (isCentered) {
-      document.body.classList.add('no-scroll');
-      document.body.addEventListener('wheel', handleWheel);
-    } else {
-      document.body.classList.remove('no-scroll');
-      document.body.removeEventListener('wheel', handleWheel);
-    }
-  }, [isCentered]);
+  // useEffect(() => {
+  //   if (isCentered) {
+  //     document.body.classList.add('no-scroll');
+  //     document.body.addEventListener('wheel', handleWheel);
+  //   } else {
+  //     document.body.classList.remove('no-scroll');
+  //     document.body.removeEventListener('wheel', handleWheel);
+  //   }
+  // }, [isCentered]);
 
   return (
     <section className='side-wrapper py-[80px] md:py-[124px] '>
       <Description />
 
-      <div
+      {/* <div
         id='mapWrapper'
         ref={ref}
         className='relative hidden xl:block hidden-scroll overflow-y-auto xl:h-[444px]  mt-[80px] md:mt-[56px]'
@@ -92,10 +92,10 @@ const ProblemSection = () => {
             veterans.
           </p>
         </div>
-      </div>
-      <div className='relative block xl:hidden mt-[80px] md:mt-[56px]'>
+      </div> */}
+      <div className='relative mt-[80px] md:mt-[56px]'>
         <div>
-          <MapFilled />
+          <Map />
         </div>
 
         <div className='pt-[56px] flex justify-center'>
